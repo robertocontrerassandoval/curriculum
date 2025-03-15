b18116e292f225c0b361a16dd7021585b7968762
Roberto Contreras <contreras_sandoval@hotmail.com>
2025-03-15 18:17:45 -0300

    :heavy_plus_sign: actualizacion views experiencia laboral

--- frontend/src/views/SobreMi.jsx
+++ frontend/src/views/SobreMi.jsx
@@ -3,7 +3,27 @@ import React from 'react';
 const SobreMi = () => {
     return (
         <div>
-            <h1>Sobre mí</h1>
+            <div className="background-container">
+            <section id="experience">
+  <h2>Experiencia Laboral</h2>
+  
+
+
+  <div class="job">
+    <h3>Desarrollador Web Full Stack</h3>
+    <p class="company">Marzo 2025 - Actualidad</p>
+    <ul class="responsibilities">
+      <li>Desarrollo y mantenimiento de aplicaciones web usando React.js y Node.js.</li>
+      <li>Integración de APIs para mejorar la funcionalidad de las plataformas web.</li>
+      <li>Colaboración en la creación de proyectos de software a medida.</li>
+    </ul>
+  </div>
+  
+</section>
+
+           
+            
+            </div>
         </div>
     );
 }
