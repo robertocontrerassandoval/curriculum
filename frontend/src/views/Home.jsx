4cbfce2fd20fd9c98da81f4ee9fcf14ba4839663
Roberto Contreras <contreras_sandoval@hotmail.com>
2025-03-22 20:59:38 -0300

    :tada: actualización estilos home

--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -1,10 +1,15 @@
 import React from "react";
 import imagenDePerfil from "../assets/img/imagePerfil.png";
+import { NavLink } from "react-router-dom";
 const Home = () => {
+
+  const setActiveClass = ({isActive}) => (isActive ? 'active' : 'undifined');
+
   return (
     <div>
-      <div className="background-container">
-        <div className="home">
+      <div className="home">
+       <div className="home-container">
+        <div className="home-content">
           <h4>Hola, soy</h4>
           <h1>Roberto Contreras</h1>
           <h4>Desarrollador Web Full Stack especializado en JavaScript</h4>
@@ -26,14 +31,21 @@ const Home = () => {
             aplicando tanto mis conocimientos técnicos como mi enfoque orientado
             a la solución de problemas y liderazgo.{" "}
           </p> */}
-          <div className="image-overlay">
+        </div>
+        <div className="home-buttons">
+                            <NavLink className={setActiveClass} to="/sobre-mi"> Sobre Mi </NavLink>
+
+         </div>
+          
+</div>
+        <div className="home-image">
             <img
               src={imagenDePerfil}
               alt="Imagen sobre el fondo"
-              className="overlay-image"
             />
           </div>
-        </div>
+
+
       </div>
     </div>
   );
