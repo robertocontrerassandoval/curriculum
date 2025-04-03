8e9b750c0cc6bee25aa89e44a445e43a79d1e23c
Roberto Contreras <contreras_sandoval@hotmail.com>
2025-04-03 12:09:01 -0300

    :tada:  ajustes media query home

--- frontend/src/components/NavBar.jsx
+++ frontend/src/components/NavBar.jsx
@@ -1,24 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
-import logo from '../assets/react.svg';
 
 const NavBar = () => {
+  const [menuOpen, setMenuOpen] = useState(false); // Estado para controlar la visibilidad del menú
 
-    const setActiveClass = ({isActive}) => (isActive ? 'active' : 'undifined');
-    return (
-        <div>
-        <nav className="navbar">
-            <div className="left">
-                <NavLink className={setActiveClass} to="/"> RC  </NavLink>
-            </div>
-            <div className="right">
-                <NavLink className={setActiveClass} to="/sobre-mi"> Sobre Mi </NavLink>
-                <NavLink className={setActiveClass} to="/curriculum"> Curriculum </NavLink>
-                <NavLink className={setActiveClass} to="/contacto"> Contacto </NavLink>
-            </div>
-        </nav>
+  const setActiveClass = ({ isActive }) => (isActive ? 'active' : 'undefined');
+
+  // Función para alternar la visibilidad del menú
+  const toggleMenu = () => {
+    setMenuOpen(prevState => !prevState);
+  };
+
+  return (
+    <div>
+      <nav className="navbar">
+        <div className="left">
+          <NavLink className={setActiveClass} to="/">RC</NavLink>
+        </div>
+
+        {/* Icono de la hamburguesa */}
+        <div className="hamburger" onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+
+        {/* Menú de navegación */}
+        <div className={`right ${menuOpen ? 'show' : ''}`}>
+          <NavLink className={setActiveClass} to="/sobre-mi">Sobre Mi</NavLink>
+          <NavLink className={setActiveClass} to="/curriculum">Curriculum</NavLink>
+          <NavLink className={setActiveClass} to="/contacto">Contacto</NavLink>
+        </div>
+      </nav>
     </div>
-    );
-}
+  );
+};
 
 export default NavBar;
--- frontend/src/views/Home.jsx
+++ frontend/src/views/Home.jsx
@@ -13,24 +13,6 @@ const Home = () => {
           <h4>Hola, soy</h4>
           <h1>Roberto Contreras</h1>
           <h4>Desarrollador Web Full Stack especializado en JavaScript</h4>
-          {/* <p>
-            Soy Técnico Eléctrico con 15 años de experiencia en el ámbito
-            habitacional e industrial, especializado en instalaciones
-            domiciliarias. A lo largo de mi carrera, he trabajado en diversos
-            proyectos, tanto residenciales como industriales, liderando equipos
-            de trabajo y generando resultados exitosos en la instalación,
-            mantenimiento y reparación de sistemas eléctricos. Mi enfoque
-            siempre ha sido garantizar la seguridad, eficiencia y calidad en
-            cada proyecto. Hace un año, decidí dar un giro a mi carrera
-            profesional y me embarqué en el estudio de Desarrollo Web Full Stack
-            JavaScript, donde he adquirido habilidades clave como HTML, CSS,
-            JavaScript, React, y Node.js, así como el uso de bases de datos como
-            Postgres y herramientas de control de versiones como Git. Con una
-            base sólida en el desarrollo de aplicaciones web dinámicas y
-            escalables, mi objetivo es seguir creciendo como desarrollador,
-            aplicando tanto mis conocimientos técnicos como mi enfoque orientado
-            a la solución de problemas y liderazgo.{" "}
-          </p> */}
         </div>
         <div className="home-buttons">
                             <NavLink className={setActiveClass} to="/sobre-mi"> Sobre Mi </NavLink>
